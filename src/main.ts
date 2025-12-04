9e920cb9c29fb88df05c660f5abb35e5e70cbed2
rukhman <aleksei.rukhmanov@gmail.com>
2025-12-05 00:39:57 +0300

    о

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://rukhmanov-kwadro-frontend-877a.twc1.net',
+      'https://motomarket52r.ru',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
